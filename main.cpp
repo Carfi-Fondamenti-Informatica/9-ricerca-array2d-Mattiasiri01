af69016f8ccd3479590b70a4a9e80e2b03c04954
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-05-15 20:15:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+
+int main() {
+
+    // Dichiarazione
+    char list1[10][20];
+    char name[20];
+
+    // Inizializzazione
+    inserimento(list1);
+    inserimento(name);
+    cout << confronto(list1, name)+1;
 }
